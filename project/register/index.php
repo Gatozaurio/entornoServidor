15fd2b5ef271d1c0c2c0a73051d0dd566cc5e709
Gatozaurio <tavoraromero@gmail.com>
2018-11-07 11:51:19 +0100

    Validaciones

--- project/register/index.php
+++ project/register/index.php
@@ -1,19 +1,54 @@
 <?php
-if(!isset($_POST['register']) ){
+if(isset($_POST['register']) ){
+
     $name = $_POST['name'] ?? null;
-    $name = $_POST['email'] ?? null;
-    $name = $_POST['password'] ?? null;
-    $name = $_POST['password_confirmation'] ?? null;
+    $email = $_POST['email'] ?? null;
+    $password = $_POST['password'] ?? null;
+    $password_confirmation = $_POST['password_confirmation'] ?? null;
 
     $errores =  [];
     // Validar los datos
     if( is_null($name) || $name=="" ){
-        $errores['name'] = 'El campo nombre es obligatorio';
-    }else if( $name)
-    if( !empty($errores) ){
+        $errores['name']['empty'] = 'El campo nombre es obligatorio.';
+    }
+    if( is_null($email) || $email=="" ){
+        $errores['email']['empty'] = 'El campo email es obligatorio.';
+    }
+    if( is_null($password) || $password=="" ){
+        $errores['password']['empty'] = 'El campo contraseña es obligatorio.';
+    }
+    if( is_null($password_confirmation) || $password_confirmation=="" ){
+        $errores['password_confirmation']['empty'] = 'La confirmación de la contraseña es obligatoria.';
+    }
+    // Validación nombre
+    if( strlen($name)<8){
+        $errores['name']['too_short'] = 'El nombre de usuario debe tener al menos 8 caracteres.';
+    }
+    if( !preg_match("/^[a-z0-9]+$/",$name)){
+        $errores['name']['no_valid'] = 'Los nombres sólo pueden contener eltras minúsculas y números.';
+    }
+    // Validación email
+    if( !preg_match("/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/",$email)){
+        $errores['email']['no_valid'] = 'Debe introducir un email válido.';
+    }
+    if( strlen($password)<6){
+        $errores['password']['too_short'] = 'La contraseña debe tener al menos 6 caracteres.';
+    }
+    if($password != $password_confirmation){
+        $errores['password_confirmation']['not_equals'] = 'Las contraseñas no coinciden.';
+    }
+    if( !isset($errores) ){
+        echo "nombre: {$name} <br />";
+        echo "nombre: {$email} <br />";
+        echo "nombre: {$password} <br />";
+        echo "nombre: {$password_confirmation} <br />";
+    } else {
         require_once 'register.view.php';
     }
+
 }else{
     require_once 'register.view.php';
-}
+} 
+
+
 ?>
--- project/register/register.view.php
+++ project/register/register.view.php
@@ -10,28 +10,57 @@
             <form action="" method="post">
             <div class="form-group">
                 <label for="name">Nombre</label>
-                <input type="text" class="form-control"
-                <?php (isset($errores['name']) )? "is-invalid":"" ?>
-                id="name" name="name" aria-describedby="nameHelp" placeholder="Introduce un nombre de usuario">
+                <input type="text" class="form-control <?php (isset($errores['name']) )? "is-invalid":"" ?>" id="name" name="name" aria-describedby="nameHelp" placeholder="Introduce un nombre de usuario">
                 <small id="nameHelp" class="form-text text-muted">El nombre con el que te identificarás en la página web.</small>
                 <?php if( isset($errores['name']) ): ?>
                     <div class="invalid-feedback">
-                        El nombre es obligatorio.
+                        <ul>
+                            <?php foreach ($error['name'] as $message): ?>
+                                <li><?=$message?></li>
+                            <?php endforeach; ?>
+                        </ul>
                     </div>
                 <?php endif; ?>
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Introduce un email">
+                <input type="email" class="form-control <?php (isset($errores['email']) )? "is-invalid":"" ?>" id="email" name="email" placeholder="Introduce un email">
                 <small id="emailHelp" class="form-text text-muted">Prometemos no hacer cosas malas con tu email.</small>
+                <?php if( isset($errores['name']) ): ?>
+                    <div class="invalid-feedback">
+                        <ul>
+                            <?php foreach ($error['name'] as $message): ?>
+                                <li><?=$message?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
             </div>
             <div class="form-group">
                 <label for="password">Contraseña</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Introduce tu contraseña">
+                <input type="password" class="form-control <?php (isset($errores['password']) )? "is-invalid":"" ?>" id="password" name="password" placeholder="Introduce tu contraseña">
+                <?php if( isset($errores['password']) ): ?>
+                    <div class="invalid-feedback">
+                        <ul>
+                            <?php foreach ($error['password'] as $message): ?>
+                                <li><?=$message?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
             </div>
             <div class="form-group">
                 <label for="password_confirmation">Confirma contraseña</label>
-                <input type="password" class="form-control" id="'password_confirmation'" name="password" placeholder="Confirma tu contraseña">
+                <input type="password" class="form-control <?php (isset($errores['password_confirmation']) )? "is-invalid":"" ?>" id="'password_confirmation'" name="password" placeholder="Confirma tu contraseña">
+                <?php if( isset($errores['password_confirmation']) ): ?>
+                    <div class="invalid-feedback">
+                        <ul>
+                            <?php foreach ($error['password_confirmation'] as $message): ?>
+                                <li><?=$message?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
             </div>
             <button type="submit" class="btn btn-primary" name="register">Enviar</button>
             </form>
